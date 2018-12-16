55693b7e0b5e77365133f41d1c5cbe24946423f9
dirtyread <dirtyread@outlook.com>
2018-12-16 02:37:39 +0100

    4: add visualization and callback function

--- deep-nn.py
+++ deep-nn.py
@@ -7,6 +7,10 @@ from keras.layers import Dense
 from keras.optimizers import Adam
 # my utility function
 import my_utils_function as muf
+# for visualization
+from keras.utils import plot_model
+# to callback function - early stopping when train
+from keras.callbacks import EarlyStopping
 
 
 X, y = make_circles(n_samples=1000, factor=.6, noise=.1, random_state=42)
@@ -20,8 +24,13 @@ model = Sequential()
 model.add(Dense(4, input_shape=(2,), activation="tanh", name="hidden_1"))
 model.add(Dense(4, activation="tanh", name="hidden_2"))
 model.add(Dense(1, activation="sigmoid", name="output_layer"))
+# visualization
+model.summary()
 model.compile(Adam(lr=.05), 'binary_crossentropy', metrics=['accuracy'])
-model.fit(X_train, y_train, epochs=100, verbose=1, validation_data=(X_test, y_test))
+# visualization
+plot_model(model, to_file="model.png", show_shapes=True, show_layer_names=True)
+my_callbacks = [EarlyStopping(monitor="val_acc", patience=5, mode='max')]
+model.fit(X_train, y_train, epochs=100, verbose=1, callbacks=my_callbacks, validation_data=(X_test, y_test))
 
 evaluate_result = model.evaluate(X_test, y_test)
 print(f"loss:{evaluate_result[0]}\naccuracy:{evaluate_result[1]}")
